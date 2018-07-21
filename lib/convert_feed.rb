bbc9022f5f413d56907ea164e6d91cbd0e4d2897
Alexandr Savin <savin@restream.rt.ru>
2018-07-21 20:40:04 +0400

    style fix

--- lib/convert_feed.rb
+++ lib/convert_feed.rb
@@ -20,12 +20,13 @@ class ConverterFeed
       Downloader::Http.new(source),
       Downloader::Stdin.new(source)
     ]
-    downloader = kinds.find(&:usable?)
 
+    downloader = kinds.find(&:usable?)
     parser = Parser::Atom.new
     builder = Converter::Rss.new # options[:output]
 
     xml = converter(downloader, parser, builder)
+
     STDOUT.puts xml if @options[:output]
 
     xml
